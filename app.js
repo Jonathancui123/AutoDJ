2cf1126e051e4401980ddac4cb79665ff5d7c900
Jonathan Cui <jonathancui123@gmail.com>
2020-01-06 16:54:43 -0500

    updated app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const request = require('request');
 
 const clientId = '158a4f4cd2df4c9e8a8122ec6cc3863a';
+
 const clientSecret = process.env.clientSecret;
 // const redirectUri = 
 
@@ -28,8 +30,19 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/loggedin', (req, res) => {
+    var code = req.query.code;
+    request.post('https://accounts.spotify.com/api/token', {
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: "http://localhost:3000/loggedin",
+        client_id: clientId,
+        client_secret: clientSecret
+    }, (err, httpResponse, body) => {
+        console.log(err);
+    })
     res.sendFile(path.join(__dirname + '/views/loggedin.html'));
 })
+
 
 // TODO: Rejected login handling
 
