7041a3d74949ecc2e5edb4853611faf9bf067188
oustan10 <oustan10@gmail.com>
2020-01-07 16:52:26 -0500

    first draft of users.js

--- server/users.js
+++ server/users.js
@@ -14,14 +14,13 @@ function registerUser(code) {
         } else {
             const info = JSON.parse(body);
             // Get current date and time
-            const today = new Date();
-            const currentDateTime = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':';
+            const now = new Date();
             users.push(new User(
                 Math.max.apply(Math, users.map(user => { return user.id; })),
                 info.display_name,
                 info.id,
                 users.map(user => { return user.role; }).includes('host') ? 'guest' : 'host',
-                currentDateTime
+                now
             ));
             console.log('Current users', users);
         }
@@ -34,6 +33,82 @@ function getSongs(code) {
         method: 'GET',
         headers: {
             'Authorization': code
+        },
+        body: 'limit=20'
+    }, (err, res, body) => {
+        if (err) {
+            console.log('Error returned!');
+        } else {
+            var returnedSongs = res.items.sort((a, b) => {
+                if (a.popularity < b.popularity) {
+                    return 1;
+                }
+                if (a.popularity > b.popularity) {
+                    return -1;
+                }
+                return 0;
+            });
         }
-    })
+        var i = 0;
+        var matches = 0;
+        while (matches < 3 && i < 20) {
+            var genres = genreLookup(code, returnedSongs[i].artists[0]);
+            if (genres.includes(selectedGenre)) {
+                if (songBankLookup(returnedSongs[i].uri) >= 0) {
+                    songBank[i].score++;
+                } else {
+                    songBank.push(new Song(
+                        Math.max.apply(Math, users.map(user => { return user.id; })),
+                        returnedSongs[i].name,
+                        returnedSongs[i].artists[0],
+                        genres,
+                        1,
+                        false,
+                        returnedSongs[i].uri
+                    ));
+                }
+            }
+        }
+    });
 }
+
+function genreLookup(code, artist) {
+    request({
+        url: `https://api.spotify.com/v1/artists/${artist.id}`,
+        method: 'GET',
+        headers: {
+            Authorization: code
+        }
+    }, (err, res, body) => {
+        return res.genres;
+    });
+}
+
+function songBankLookup(uri) {
+    for (let i = 0; i < songBank.length; i++) {
+        if (uri === song.link) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function autoKick() {
+    const now = new Date();
+    var i = 0;
+    while (users[i]) {
+        if (now - users[i].joinTime > (60 * 60 * 1000)) {
+            users.splice(i, 1);
+        } else {
+            i++;
+        }
+    };
+}
+
+module.exports = {
+    registerUser: registerUser,
+    getSongs: getSongs,
+    genreLookup: genreLookup,
+    songBankLookup: songBankLookup,
+    autoKick: autoKick
+};
