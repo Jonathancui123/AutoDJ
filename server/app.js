6c8bd87fb4c526494ec9b78aea916af06e6ac7ce
oustan10 <oustan10@gmail.com>
2020-01-07 17:21:45 -0500

    debugging spotify api calls

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const request = require('request');
-const userHelpers = require('./users');
+// const userHelpers = require('./users');
 const queueHelpers = require('./q');
 
 const clientId = '158a4f4cd2df4c9e8a8122ec6cc3863a';
@@ -78,6 +78,7 @@ app.get('/loggedin', (req, res) => {
         console.log("refresh token: " + refresh_token)
         setInterval(refresh_access, (58*60000)); // Refreshes token every 58 minutes, it expires every 60
     })
+    registerUser(code);
     res.sendFile(path.join(__dirname + '/views/loggedin.html'));
 })
 
@@ -108,6 +109,112 @@ function refresh_access() {
 // HELPER FUNCTIONS
 ///////////////////////////////////////////////
 
+function registerUser(code) {
+    // Get user info
+    request({
+        url: 'https://api.spotify.com/v1/me',
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + code
+        }
+    }, (err, res, body) => {
+        if (err) {
+            console.log('Response error');
+        } else {
+            const info = JSON.parse(body);
+            console.log('Response ', info);
+            // Get current date and time
+            const now = new Date();
+            users.push(new User(
+                Math.max.apply(Math, users.map(user => { return user.id; })),
+                info.display_name,
+                info.id,
+                users.map(user => { return user.role; }).includes('host') ? 'guest' : 'host',
+                now
+            ));
+            console.log('Current users', users);
+        }
+    });
+}
+
+function getSongs(code) {
+    request({
+        url: 'https://api.spotify.com/v1/me/top/tracks',
+        method: 'GET',
+        headers: {
+            'Authorization': code
+        },
+        body: 'limit=20'
+    }, (err, res, body) => {
+        if (err) {
+            console.log('Error returned!');
+        } else {
+            var returnedSongs = res.items.sort((a, b) => {
+                if (a.popularity < b.popularity) {
+                    return 1;
+                }
+                if (a.popularity > b.popularity) {
+                    return -1;
+                }
+                return 0;
+            });
+        }
+        var i = 0;
+        var matches = 0;
+        while (matches < 3 && i < 20) {
+            var genres = genreLookup(code, returnedSongs[i].artists[0]);
+            if (genres.includes(selectedGenre)) {
+                if (songBankLookup(returnedSongs[i].uri) >= 0) {
+                    songBank[i].score++;
+                } else {
+                    songBank.push(new Song(
+                        Math.max.apply(Math, users.map(user => { return user.id; })),
+                        returnedSongs[i].name,
+                        returnedSongs[i].artists[0],
+                        genres,
+                        1,
+                        false,
+                        returnedSongs[i].uri
+                    ));
+                }
+            }
+        }
+    });
+}
+
+function genreLookup(code, artist) {
+    request({
+        url: `https://api.spotify.com/v1/artists/${artist.id}`,
+        method: 'GET',
+        headers: {
+            Authorization: code
+        }
+    }, (err, res, body) => {
+        return res.genres;
+    });
+}
+
+function songBankLookup(uri) {
+    for (let i = 0; i < songBank.length; i++) {
+        if (uri === song.link) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function autoKick() {
+    const now = new Date();
+    var i = 0;
+    while (users[i]) {
+        if (now - users[i].joinTime > (60 * 60 * 1000)) {
+            users.splice(i, 1);
+        } else {
+            i++;
+        }
+    };
+}
+
 // Make a array of song URIs (by descending order of score) until playlist length is equal to requested length --> CASE: if there are more songs needed than in the bank
 // TODO: Order the songs by BPM/Pitch/Something useful
 // TODO: Perform a check to see if the saved spotify ID exists as a playlist
@@ -119,3 +226,13 @@ function refresh_access() {
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
 })
+
+// module.exports = {
+//     songBank: songBank,
+//     users: users,
+//     selectedGenre: selectedGenre,
+//     playlistDur: playlistDur,
+//     playlistID: playlistID,
+//     Song: Song,
+//     User: User
+// }
