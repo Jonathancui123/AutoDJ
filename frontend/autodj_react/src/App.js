ed827073efb370381fb6f429bd939b0dff9dcb99
Jonathan Cui <jonathancui123@gmail.com>
2020-01-15 19:20:08 -0500

    Front end restart

---
+++ frontend/autodj_react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './new_logo.png';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <div className="content-center brand">
+            <h1 className="h1-seo">AutoDJ</h1>
+            <h3 className="d-none d-sm-block">
+            Using spotify profiles to curate a playlist perfect for your party
+              </h3>
+        </div>
+
+        <Button variant="primary"> Start building your playlist! </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
